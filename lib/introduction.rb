c36043a6db9cf6ed95f7813801d23bfc51dfee5d
Jonathan Snipe <jon.mike919@gmail.com>
2020-05-28 20:17:06 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,13 @@
+def introduction(name) 
+end
+ 
+ def introduction(name)
+   puts "hi, my name is #{name}."
+ end
+ 
+ def introduction_with_language(name,language)
+   puts "Hi, my name is #{name} and I am learning to program in #{language}."
+   
+   
+   
+ 
